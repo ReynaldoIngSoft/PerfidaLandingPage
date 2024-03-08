cd2ff7483842e8faa80dd5b507c9a0801547bbaf
ReynaldoIngSoft <reynaldodelriocontreras@gmail.com>
2024-03-08 12:43:16 -0500

    carrito

---
+++ assets/js/detail.js
@@ -0,0 +1,39 @@
+import products from "./products.js";
+import cart from "./cart.js";
+let app = document.getElementById('app');
+let temporaryContent = document.getElementById('temporaryContent');
+
+
+const loadTemplate = () =>{
+     fetch('/cartPerfida.html')
+     .then(response => response.text())
+     .then(html => {
+        app.innerHTML = html;
+        let contentTab = document.getElementById('contentTab');
+        contentTab.innerHTML = temporaryContent.innerHTML;
+        temporaryContent.innerHTML = null;
+        cart();
+        initApp();
+     })
+}
+
+loadTemplate();
+const initApp = () => {
+    let idProduct = new  URLSearchParams(window.location.search).get('id');
+    let info = products.filter((value) => value.id == idProduct)[0];
+    console.log(info);
+    if(!info){
+        window.location.href='/cartPerfida.html';
+    }
+    let detail = document.querySelector('.detail');
+    detail.querySelector('.image img').src =info,image;
+    detail.querySelector('.name').innerText = info.name;
+    detail.querySelector('.price').innerText = 'S/.'+info.price;
+    detail.querySelector('.description').innerText = info.description;
+    detail.querySelector('.addCart').dataset.id = idProduct;
+
+
+
+
+}
+
---
+++ assets/js/products.js
@@ -0,0 +1,69 @@
+ const products = [
+    {
+        "id":1,
+        "name": "LENCERIA BONITA 2",
+        "price": 180,
+        "image": "./assets/images/oferta1.jpg",
+        "description":"lenceria bonitañl,sldmlñasmñldmaslñ sa kdln asklndklas ,la kldaksl ,mas kldmask dm asmld asklm dklmaskld kla kld askld dklas kld akl "
+      },
+  
+      {
+          "id":2,
+          "name": "LENCERIA BONITA 2",
+          "price": 180,
+          "image": "./assets/images/compri3.jpg",
+          "description":"lenceria bonitañl,sldmlñasmñldmaslñ sa kdln asklndklas ,la kldaksl ,mas kldmask dm asmld asklm dklmaskld kla kld askld dklas kld akl "
+        },
+        {
+          "id":3,
+          "name": "LENCERIA BONITA 2",
+          "price": 180,
+          "image": "./assets/images/compri3.jpg",
+          "description":"lenceria bonitañl,sldmlñasmñldmaslñ sa kdln asklndklas ,la kldaksl ,mas kldmask dm asmld asklm dklmaskld kla kld askld dklas kld akl "
+        },
+        {
+          "id":4,
+          "name": "LENCERIA BONITA 2",
+          "price": 180,
+          "image": "./assets/images/compri3.jpg",
+          "description":"lenceria bonitañl,sldmlñasmñldmaslñ sa kdln asklndklas ,la kldaksl ,mas kldmask dm asmld asklm dklmaskld kla kld askld dklas kld akl "
+        },
+        {
+          "id":5,
+          "name": "LENCERIA BONITA 2",
+          "price": 180,
+          "image": "./assets/images/compri3.jpg",
+          "description":"lenceria bonitañl,sldmlñasmñldmaslñ sa kdln asklndklas ,la kldaksl ,mas kldmask dm asmld asklm dklmaskld kla kld askld dklas kld akl "
+        },
+        {
+          "id":6,
+          "name": "LENCERIA BONITA 2",
+          "price": 180,
+          "image": "./assets/images/compri3.jpg",
+          "description":"lenceria bonitañl,sldmlñasmñldmaslñ sa kdln asklndklas ,la kldaksl ,mas kldmask dm asmld asklm dklmaskld kla kld askld dklas kld akl "
+        },
+        {
+          "id":7,
+          "name": "LENCERIA BONITA 2",
+          "price": 180,
+          "image": "./assets/images/compri3.jpg",
+          "description":"lenceria bonitañl,sldmlñasmñldmaslñ sa kdln asklndklas ,la kldaksl ,mas kldmask dm asmld asklm dklmaskld kla kld askld dklas kld akl "
+        },
+        {
+          "id":8,
+          "name": "LENCERIA BONITA 2",
+          "price": 180,
+          "image": "./assets/images/compri3.jpg",
+          "description":"lenceria bonitañl,sldmlñasmñldmaslñ sa kdln asklndklas ,la kldaksl ,mas kldmask dm asmld asklm dklmaskld kla kld askld dklas kld akl "
+        },
+        {
+          "id":9,
+          "name": "LENCERIA BONITA 2",
+          "price": 180,
+          "image": "./assets/images/compri3.jpg",
+          "description":"lenceria bonitañl,sldmlñasmñldmaslñ sa kdln asklndklas ,la kldaksl ,mas kldmask dm asmld asklm dklmaskld kla kld askld dklas kld akl "
+        }
+
+];
+
+export default products;
