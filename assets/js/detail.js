88657884fc0e561ac1687708d9f07c2c7edf2f7e
ReynaldoIngSoft <reynaldodelriocontreras@gmail.com>
2024-03-12 17:58:02 -0500

    imagencuadrada

--- assets/js/detail.js
+++ assets/js/detail.js
@@ -1,5 +1,6 @@
 import products from "./products.js";
 import cart from "./cart.js";
+
 let app = document.getElementById('app');
 let temporaryContent = document.getElementById('temporaryContent');
 
@@ -18,6 +19,8 @@ const loadTemplate = () =>{
 }
 
 loadTemplate();
+
+
 const initApp = () => {
     let idProduct = new  URLSearchParams(window.location.search).get('id');
     let info = products.filter((value) => value.id == idProduct)[0];
@@ -26,7 +29,7 @@ const initApp = () => {
         window.location.href='/cartPerfida.html';
     }
     let detail = document.querySelector('.detail');
-    detail.querySelector('.image img').src =info,image;
+    detail.querySelector('.image img').src =info.image;
     detail.querySelector('.name').innerText = info.name;
     detail.querySelector('.price').innerText = 'S/.'+info.price;
     detail.querySelector('.description').innerText = info.description;
